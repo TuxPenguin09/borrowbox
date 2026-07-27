d5e69a11969142fc8747f1ed036218a744b05846
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2026-07-27 18:08:48 +0800

    refractor: URL routing something (unfinished)

--- borrowbox/frontend/urls.py
+++ borrowbox/frontend/urls.py
@@ -1,6 +1,22 @@
+from pathlib import Path
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
-urlpatterns = [
-    path("", views.index, name="index"),
-]
+TEMPLATE_DIR = Path(__file__).resolve().parent / "templates"
+
+urlpatterns = []
+
+for html in TEMPLATE_DIR.glob("*.html"):
+    name = html.stem
+
+    view = TemplateView.as_view(template_name=html.name)
+
+    if name == "index":
+        # index is always the home
+        urlpatterns.append(path("", view, name="index"))
+        urlpatterns.append(path("index.html", view))
+    else:
+        # .html included
+        urlpatterns.append(path(f"{name}/", view, name=name))
+        urlpatterns.append(path(f"{name}.html", view))
